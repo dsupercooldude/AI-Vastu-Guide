d27dc1267af3c43b11e13d848239f820278e1e15
dsupercooldude <61223794+dsupercooldude@users.noreply.github.com>
2026-09-11 13:54:59 +0530

    chore: remove temporary migration patches
    Clean up project directory by removing unused `.cjs` migration scripts that are no longer required.

--- patch_firebase.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/firebase.ts', 'utf8');
-code = code.replace("import { getStorage } from 'firebase/storage';\n", "");
-code = code.replace("export const storage = getStorage(app);\n", "");
-fs.writeFileSync('src/firebase.ts', code);
